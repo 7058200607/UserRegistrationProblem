d714271c5265802008022d684316479017166c53
vrushali joshi <vrushalijoshi589@mail.com>
2022-05-30 14:45:43 +0530

    added First Name in User Regestration

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationProblem
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Regestration Using Regex");
+            UserRegestration userRegestration = new UserRegestration();
+            Console.WriteLine("Enter Your First Name :");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine(userRegestration.ValidateFirstName(FirstName));
+
+        }
+
+    }
+}
---
+++ UserRegestration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationProblem
+{
+    public class UserRegestration
+    {
+        public const string Regex_FirstName = "^[A-Z]{1}[A-Za-z]{2,}$";
+
+        public bool ValidateFirstName(string firstName) 
+        {
+            return Regex.IsMatch(firstName,Regex_FirstName);
+        }
+    }
+    
+}
